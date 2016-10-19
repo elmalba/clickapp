5cfed8db1bdd7c340ccabaf3860d0a1eb3272b32
Manuel Alba <malba@mmae.cl>
2016-10-19 14:35:24 -0300

    HOALA

--- app/controllers/ppt/live_controller.rb
+++ app/controllers/ppt/live_controller.rb
@@ -3,4 +3,12 @@ class Ppt::LiveController < ActionController::Base
   def show
     @question=Question.find(params[:id])
   end
+  def index
+    answer=Answer.new()
+    answer.answer=params[:ans]
+    answer.save()
+
+    render plain:"ok"
+
+  end
 end
